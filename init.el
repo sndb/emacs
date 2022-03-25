f876fd4fcd0ec481e50a7979f1e1a46b683576d8
sndb <sndb@sndb.xyz>
2022-03-25 20:51:35 +0300

    rm org-appear, add consult, vertico, savehist, marginalia, affe, vterm

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding: t -*-
+
 ;; packages
 
 (require 'package)
@@ -84,10 +86,9 @@
 
 (use-package which-key
   :init
-  (which-key-mode)
-  (which-key-setup-minibuffer)
+  (setq which-key-idle-delay 0.25)
   :config
-  (setq which-key-idle-delay 0.3))
+  (which-key-mode))
 
 (use-package centered-cursor-mode
   :config
@@ -100,6 +101,8 @@
   (setq evil-want-keybinding nil
         evil-undo-system 'undo-fu)
   :config
+  (evil-global-set-key 'normal (kbd "SPC f") 'affe-find)
+  (evil-global-set-key 'normal (kbd "SPC g") 'affe-grep)
   (evil-mode 1))
 
 (use-package evil-collection
@@ -131,9 +134,75 @@
 (use-package org-superstar
   :hook (org-mode . org-superstar-mode))
 
-(use-package org-appear
-  :hook (org-mode . org-appear-mode)
+;; completion
+
+(use-package consult
+  :bind (;; C-c bindings (mode-specific-map)
+         ("C-c h" . consult-history)
+         ("C-c m" . consult-mode-command)
+         ("C-c k" . consult-kmacro)
+         ;; C-x bindings (ctl-x-map)
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ("<help> a" . consult-apropos)            ;; orig. apropos-command
+         ;; M-g bindings (goto-map)
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings (search-map)
+         ("M-s d" . consult-find)
+         ("M-s D" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s m" . consult-multi-occur)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+)
+
+(use-package vertico
   :init
-  (setq org-hide-emphasis-markers t
-        org-appear-autolinks t
-        org-appear-autosubmarkers t))
+  (vertico-mode))
+
+(use-package savehist
+  :init
+  (savehist-mode))
+
+(use-package marginalia
+  :init
+  (marginalia-mode))
+
+(use-package affe)
+
+;; also
+
+(use-package vterm)
