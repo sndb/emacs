c10213f8257f709d0f28b229b5150434d968c047
sndb <sndb@sndb.xyz>
2023-07-18 17:34:57 +0300

    increase font size

--- sndb-modules/sndb-appearance.el
+++ sndb-modules/sndb-appearance.el
@@ -15,7 +15,7 @@
 ;;;; Fonts
 (setq text-scale-mode-step 1.1)
 (mapc (lambda (face)
-        (set-face-attribute face nil :font "Hack:pixelsize=14"))
+        (set-face-attribute face nil :family "Hack" :height 110))
       '(default fixed-pitch))
 
 ;;;; Indicators
