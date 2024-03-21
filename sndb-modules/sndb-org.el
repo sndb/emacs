bb1db619811dfad39bd67d7ca95ca25f04749536
sndb <sndb@sndb.xyz>
2024-03-21 17:21:43 +0300

    simplify org module

--- sndb-modules/sndb-org.el
+++ sndb-modules/sndb-org.el
@@ -1,56 +1,24 @@
+;;;; General
 (require 'org)
-(require 'org-agenda)
-(setq org-modules '(org-id ol-info))
 
-;;;; Source
-(setq org-src-window-setup 'current-window)
-(setq org-src-preserve-indentation t)
-(setq org-confirm-babel-evaluate nil)
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((python . t)
-   (shell . t)
-   (emacs-lisp . t)
-   (C . t)
-   (latex . t)))
-
-;;;; UI
 (setq org-directory "~/data/notes")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 (setq org-startup-folded 'showall)
-(setq org-catch-invisible-edits 'error)
-(setq org-return-follows-link t)
-(setq org-id-link-to-org-use-id 'create-if-interactive)
-(setq org-list-allow-alphabetical t)
-(setq org-ellipsis "↷")
-(setq org-export-with-author nil)
-
-(add-hook 'org-mode-hook #'visual-line-mode)
-
-;;;; LaTeX
-(setq org-highlight-latex-and-related '(latex))
-(setq org-startup-with-latex-preview t)
-(setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
-(setq org-latex-preview-ltxpng-directory (concat user-emacs-directory "ltximg/"))
-
-;;;; Tasks
-(setq org-archive-location (concat org-directory "/archive.org::"))
+(setq org-fold-catch-invisible-edits 'error)
 (setq org-enforce-todo-dependencies t)
 (setq org-enforce-todo-checkbox-dependencies t)
-(setq org-log-done 'time)
-(setq org-log-into-drawer t)
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)")))
-(setq org-use-fast-todo-selection 'expert)
+
+(keymap-global-set "C-c a" #'org-agenda)
+(keymap-global-set "C-c c" #'org-capture)
 
 ;;;; Agenda
+(require 'org-agenda)
+
 (setq org-agenda-window-setup 'current-window)
 (setq org-agenda-files `(,org-directory))
-(setq org-agenda-dim-blocked-tasks t)
-(setq org-agenda-todo-ignore-scheduled 'future)
-(setq org-agenda-start-on-weekday nil)
+(setq org-agenda-todo-ignore-with-date t)
 (setq org-agenda-skip-deadline-prewarning-if-scheduled 'pre-scheduled)
-(setq org-agenda-skip-scheduled-if-done t)
+
 (keymap-set org-mode-map "C-c y" #'org-todo-yesterday)
 (keymap-set org-agenda-mode-map "C-c y" #'org-agenda-todo-yesterday)
 
@@ -61,15 +29,24 @@
          "* TODO %?\n%u\n%i"
          :empty-lines 1)))
 
-;;;; Refiling
+;;;; Refile
 (setq org-refile-targets
       '((org-agenda-files . (:maxlevel . 3))
         (nil . (:maxlevel . 3))))
 (setq org-refile-use-outline-path 'file)
-(setq org-outline-path-complete-in-steps nil)
 
-(keymap-global-set "C-c l" #'org-store-link)
-(keymap-global-set "C-c a" #'org-agenda)
-(keymap-global-set "C-c c" #'org-capture)
+;;;; Source
+(setq org-src-window-setup 'current-window)
+(setq org-src-preserve-indentation t)
+(setq org-confirm-babel-evaluate nil)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ (mapcar (lambda (a) (cons a t))
+         '(C emacs-lisp latex python shell sql)))
+
+;;;; LaTeX
+(setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
+(setq org-preview-latex-image-directory (concat user-emacs-directory "ltximg/"))
 
 (provide 'sndb-org)
