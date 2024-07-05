2c916cc3665de98f9e994e03ae05bc10baed5f7c
sndb <sndb@sndb.xyz>
2024-07-05 19:04:28 +0300

    prevent garbage collection during init

--- early-init.el
+++ early-init.el
@@ -1,6 +1,8 @@
-(setq max-specpdl-size (* 4 max-specpdl-size))
-(setq max-lisp-eval-depth (* 4 max-lisp-eval-depth))
-(setq gc-cons-threshold (* 64 1024 1024))
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (expt 2 23))))
+
+(setq gc-cons-threshold most-positive-fixnum)
 (setq load-prefer-newer t)
 (setq native-comp-async-report-warnings-errors 'silent)
 
